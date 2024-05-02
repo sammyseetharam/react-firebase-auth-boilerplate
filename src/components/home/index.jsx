96fcb5153c422f4667fabbc465fe20671f4e49d8
sammyseetharam <90780132+sammyseetharam@users.noreply.github.com>
2024-05-02 14:18:46 -0500

    auth for app
    code thats specific to my firebase

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,7 +4,10 @@ import { useAuth } from '../../contexts/authContext'
 const Home = () => {
     const { currentUser } = useAuth()
     return (
-        <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
+        <div className='text-2xl font-bold pt-14'>
+            Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
+            
+            </div>
     )
 }
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,27 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyAE5bLw-SFY3gYIDf93qwIVwYwc9Xj0WjQ",
+  authDomain: "fremd-app.firebaseapp.com",
+  projectId: "fremd-app",
+  storageBucket: "fremd-app.appspot.com",
+  messagingSenderId: "628556885663",
+  appId: "1:628556885663:web:6909c9f2bbab2b44823caa",
+  measurementId: "G-2BKCW8SDBJ"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app) 
 
-
-
-export { app, auth };
+export { app,auth }; 
